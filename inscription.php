cd0ba9eb3bc955e6b63dda24bd7d2106f2968293
Ozomozyan <125055262+Ozomozyan@users.noreply.github.com>
2023-05-10 21:08:15 +0200

    Create

---
+++ inscription.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+<h1>Reception des données du formulaire</h1>
+
+<?php
+// Get form data
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$contact = $_POST['contact'];
+$adresse = $_POST['adresse'];
+$email = $_POST['email'];
+$codeAcces = password_hash($_POST['codeAcces'], PASSWORD_DEFAULT);
+
+// Connect to the database
+$base = new PDO('mysql:host=localhost;dbname=id20732448_bozudata', 'id20732448_bozu', 'C>3Gmt-4_2h3Fp)/');
+$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$base->exec("SET CHARACTER SET utf8");
+
+// Prepare and execute the query
+$sql = 'INSERT INTO client (nom, prenom, contact, adresse, email, codeAcces) VALUES (:nom, :prenom, :contact, :adresse, :email, :codeAcces)';
+$stmt = $base->prepare($sql);
+$stmt->bindParam(':nom', $nom, PDO::PARAM_STR);
+$stmt->bindParam(':prenom', $prenom, PDO::PARAM_STR);
+$stmt->bindParam(':contact', $contact, PDO::PARAM_STR);
+$stmt->bindParam(':adresse', $adresse, PDO::PARAM_STR);
+$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+$stmt->bindParam(':codeAcces', $codeAcces, PDO::PARAM_STR);
+$stmt->execute();
+
+header('Location: inscription_success.html');
+?>
+
+</br><a href="index.html">Retour à la page principale</a>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+require_once('config.php');
+
+if (isset($_POST['email']) && isset($_POST['codeAcces'])) {
+    $email = $_POST['email'];
+    $codeAcces = $_POST['codeAcces'];
+
+    // Connect to the database
+    try {
+        $base = new PDO('mysql:host=localhost;dbname=id20732448_bozudata', 'id20732448_bozu', 'C>3Gmt-4_2h3Fp)/');
+        $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $base->exec("SET CHARACTER SET utf8");
+    } catch (PDOException $e) {
+        // Handle database connection error
+        error_log("Error connecting to database: " . $e->getMessage());
+        header('Location: login.php?error=2');
+        exit();
+    }
+
+    // Prepare and execute the query
+    try {
+        $stmt = $base->prepare('SELECT * FROM client WHERE email = :email');
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($result && password_verify($codeAcces, $result['codeAcces'])) {
+            // Authentication successful
+            $_SESSION['loggedin'] = true;
+            $_SESSION['client_id'] = $result['id'];
+            $_SESSION['email'] = $result['email'];
+
+            // Redirect to the user's account page
+            header('Location: account.php');
+        } else {
+            // Authentication failed
+            header('Location: login.php?error=1');
+        }
+    } catch (PDOException $e) {
+        // Handle query error
+        error_log("Error executing query: " . $e->getMessage());
+        header('Location: login.php?error=3');
+        exit();
+    }
+} else {
+    header('Location: login.php');
+}
+?>
