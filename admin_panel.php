1d7652893d525ba20dbdf6f692508070c0a92f29
Ozomozyan <125055262+Ozomozyan@users.noreply.github.com>
2023-05-11 01:40:29 +0200

    update10

--- admin_panel.php
+++ admin_panel.php
@@ -19,19 +19,3 @@ $admin_panel_html = str_replace('{{admin_full_name}}', $admin_full_name, $admin_
 // Output the final HTML
 echo $admin_panel_html;
 ?>
-
-<form method="POST" action="add_lead.php">
-  <label for="nomEntreprise">Nom Entreprise:</label>
-  <input type="text" name="nomEntreprise" id="nomEntreprise" required>
-  <br>
-  <label for="contact">Contact:</label>
-  <input type="text" name="contact" id="contact" required>
-  <br>
-  <label for="email">Email:</label>
-  <input type="email" name="email" id="email" required>
-  <br>
-  <label for="codeAccess">Code Access:</label>
-  <input type="password" name="codeAccess" id="codeAccess" required>
-  <br>
-  <input type="submit" value="Add Lead">
-</form>
