5c95765f0c3d5bef41baa6101df67a00f7cfa923
Ozomozyan <125055262+Ozomozyan@users.noreply.github.com>
2023-05-11 14:05:48 +0200

    update33

--- fetch_cars.php
+++ fetch_cars.php
@@ -1,5 +1,16 @@
 <?php
-// Include your database connection file here...
+session_start();
+
+// Connect to the database
+try {
+    $base = new PDO('mysql:host=localhost;dbname=id20732448_bozudata', 'id20732448_bozu', 'C>3Gmt-4_2h3Fp)/');
+    $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $base->exec("SET CHARACTER SET utf8");
+} catch (PDOException $e) {
+    error_log("Error connecting to database: " . $e->getMessage());
+    header('Location: 404.html');
+    exit();
+}
 
 // Fetch all cars from the database
 $stmt = $base->prepare('SELECT id, marque, modele, annee, prix FROM voiture WHERE agenceId = :agenceId');
